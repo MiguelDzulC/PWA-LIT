ab4406be679389083e6d5857fac9202369ea11d3
MiguelDzulC <miguel.19070028@itsmotul.edu.mx>
2023-02-09 09:07:57 -0600

    my elements

--- Intro to lit/my-elements.ts
+++ Intro to lit/my-elements.ts
@@ -1,10 +1,10 @@
 import {LitElement, html, css} from 'lit';
-import {customElement, state, query} from 'lit/decorators.js';
+import {customElement, state, property, query} from 'lit/decorators.js';
 
 type ToDoItem = {
   text: string,
   completed: boolean
-}
+};
 
 @customElement('todo-list')
 export class ToDoList extends LitElement {
@@ -17,15 +17,18 @@ export class ToDoList extends LitElement {
 
   @state()
   private _listItems = [
-    { text: 'Producto 1', completed: true },
-    { text: 'Producto 2', completed: true }
+    { text: 'Make to-do list', completed: true },
+    { text: 'Complete Lit tutorial', completed: false }
   ];
+  @property()
+  hideCompleted = false;
 
   render() {
-    return html`
-      <h2>Lista</h2>
+    // TODO: Replace items definition.
+    const items = this._listItems;
+    const todos = html`
       <ul>
-        ${this._listItems.map((item) =>
+        ${items.map((item) =>
           html`
             <li
                 class=${item.completed ? 'completed' : ''}
@@ -34,14 +37,33 @@ export class ToDoList extends LitElement {
             </li>`
         )}
       </ul>
+    `;
+    // TODO: Define partial templates.
+    return html`
+      <h2>To Do</h2>
+      <!-- TODO: Update expression. -->
+      ${todos}
       <input id="newitem" aria-label="New item">
-      <button @click=${this.addToDo}>Agregar</button>
+      <button @click=${this.addToDo}>Add</button>
+      <br>
+      <label>
+        <input type="checkbox"
+          @change=${this.setHideCompleted}
+          ?checked=${this.hideCompleted}>
+        Hide completed
+      </label>
+
+
     `;
   }
 
   toggleCompleted(item: ToDoItem) {
     item.completed = !item.completed;
     this.requestUpdate();
+  }
+
+  setHideCompleted(e: Event) {
+    this.hideCompleted = (e.target as HTMLInputElement).checked;
   }
 
   @query('#newitem')
@@ -53,4 +75,3 @@ export class ToDoList extends LitElement {
     this.input.value = '';
   }
 }
-
