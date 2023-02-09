f6d4ce34dd3a7e4b3eab3aa835a23b7b5c4d5940
MiguelDzulC <miguel.19070028@itsmotul.edu.mx>
2023-02-08 19:12:14 -0600

    Intro to lit

--- Intro to lit/my-elements.ts
+++ Intro to lit/my-elements.ts
@@ -1,2 +1,11 @@
 import {LitElement, html} from 'lit';
 import {customElement} from 'lit/decorators.js';
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+  render() {
+    return html`
+      <p>Holaaaaa .</p>
+    `;
+  }
+}
