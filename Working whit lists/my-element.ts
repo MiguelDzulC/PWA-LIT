33fde8a0e5244e7720516b497fca6704877f9b22
MiguelDzulC <miguel.19070028@itsmotul.edu.mx>
2023-02-09 09:41:38 -0600

    Working whit list S3

--- Working whit lists/my-element.ts
+++ Working whit lists/my-element.ts
@@ -10,8 +10,10 @@ class MyElement extends LitElement {
     return html`
       <p>A list of names that include the letter "e"</p>
       <ul>
-        <!-- TODO: Render list items of filtered names. -->
+      ${this.names
+        .filter((name) => name.match(/e/i))
+        .map((name) => html`<li>${name}</li>`)}
       </ul>
     `;
   }
-}
+}   
