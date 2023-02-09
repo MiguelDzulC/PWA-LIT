c11b9fb5ef15b28f8ce48ce1518e1a45cccef896
MiguelDzulC <miguel.19070028@itsmotul.edu.mx>
2023-02-09 09:34:22 -0600

    Working whit list S2

--- Working whit lists/my-element.ts
+++ Working whit lists/my-element.ts
@@ -1,17 +1,17 @@
 import {LitElement, html} from 'lit';
 import {customElement, state} from 'lit/decorators.js';
-// TODO: import map directive.
+import {map} from 'lit/directives/map.js';
 
 @customElement('my-element')
 class MyElement extends LitElement {
   @state()
-  items = new Set(['Apple', 'Banana', 'Grape', 'Orange', 'Lime'])
+  items = new Set(['Objeto 1', 'Objeto 2', 'Objeto 3', 'Objeto 4'])
 
   render() {
     return html`
-      <p>My unique fruits</p>
+      <p>Mis cosas</p>
       <ul>
-        <!-- TODO: Utilize map directive to render items. -->
+        ${map(this.items, (item) => html`<li>${item}</li>`)}
       </ul>
     `;
   }
