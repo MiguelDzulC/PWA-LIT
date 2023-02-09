b2518f7595fc24775405d619f583c790839ec628
MiguelDzulC <miguel.19070028@itsmotul.edu.mx>
2023-02-09 10:27:17 -0600

    Reactivity S5

--- Reactivity/my-elements.ts
+++ Reactivity/my-elements.ts
@@ -6,6 +6,16 @@ export class MyElement extends LitElement {
   @property() forward = '';
   @property() backward = '';
 
+  willUpdate(changedProperties: PropertyValues<this>) {
+    if (changedProperties.has('forward')) {
+      this.backward = this.forward.split('').reverse().join('');
+    }
+
+    if (changedProperties.has('backward')) {
+      this.forward = this.backward.split('').reverse().join('');
+    }
+  }
+
   onInput(e: Event) {
     const inputEl = e.target as HTMLInputElement;
     if (inputEl.id === 'forward') {
