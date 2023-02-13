8073726b6bc876539dfbaf0514b3b0cdf56b9431
MiguelDzulC <miguel.19070028@itsmotul.edu.mx>
2023-02-12 19:58:11 -0600

    step7

--- Build a word viewer!/word-viewer.ts
+++ Build a word viewer!/word-viewer.ts
@@ -15,6 +15,7 @@ class WordViewer extends LitElement {
     }
   `;
 
+  // TODO: Add `playDirection` state.
   @state() private idx = 0;
   @property() words = 'initial value';
 
@@ -33,9 +34,13 @@ class WordViewer extends LitElement {
 
   render() {
     const splitWords = this.words.split('.');
+    // TODO: Update math so it won't go negatively out of bounds.
     const word = splitWords[this.idx % splitWords.length];
+    // TODO: Add @click event handler that calls `this.switchPlayDirection`
     return html`<pre>${word}</pre>`;
   }
-
+  // TODO: Increment by `this.playDirection`
   tickToNextWord = () => { this.idx += 1; };
+
+  // TODO: Add switchPlayDirection method.
 }
